95ea1823a815ee055a000a5a0bcc9791bff0a2e3
Sodkhuu1 <bsodkhuu0604@gmail.com>
2024-04-06 21:23:18 +0800

    2 udriin hoorond heden udur baigaag tootsooldog

--- hello.java
+++ hello.java
@@ -1,6 +1,29 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class hello {
+    public long daysBetween(Date one, Date two) {
+        long difference = (one.getTime() - two.getTime()) / 86400000; // 86400000 нь 1 өдөр дэх миллсекундын тоо
+        return Math.abs(difference);
+    }
+
     public static void main(String args[]) {
-        System.out.println("hello world!");
+        Date today = new Date();
+
+        Calendar myNextCalendar = Calendar.getInstance();
+        myNextCalendar.set(2020, 0, 1);
+        Date nyd = myNextCalendar.getTime();
+
+        hello myObject = new hello();
+        long days = myObject.daysBetween(today, nyd);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM dd, YYYY");
+        String todayDate = sdf.format(today);
+        String newYearsDay = sdf.format(nyd);
+
+        System.out.println(todayDate + " өдрөөс " + newYearsDay + " хүртэл нийт " + days + " өдөр бөйна.");
+
     }
 
 }
